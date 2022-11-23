bdc03aa2c41613c503b23977926b550fcd86d963
rafa <rfriasge@gmail.com>
2022-11-23 09:28:40 -0800

    First commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using PrestaFacil.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrestaFacil
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ToDo> ToDo{ get; set; }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ Models/Paginacion.cs
@@ -0,0 +1,8 @@
+﻿namespace PrestaFacil.Models
+{
+    public class Paginacion
+    {
+        public int Pagina { get; set; } = 1;
+        public int CantidadAMostrar { get; set; } = 10;
+    }
+}
